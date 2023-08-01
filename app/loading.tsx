3e827f9b8d734d0099ad5f958bc80cf6ce2d9ef6
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-08-01 10:05:50 +0300

    adding loading

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div>
+      <p className='text-4xl text-center'>Loading ...</p>
+    </div>
+  );
+};
+
+export default loading;
