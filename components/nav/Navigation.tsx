3300ba468c39762b09c3f0f642c30ae710d2603f
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-08-01 10:36:04 +0300

    modfying the nav

--- components/nav/Navigation.tsx
+++ components/nav/Navigation.tsx
@@ -16,11 +16,11 @@ const Navigation = () => {
         </div>
       </Link>
       <div className='links flex justify-between gap-3 border-2 w-5/6 px-2 py-3 mx-auto'>
-        <NavLink href='/blog'>Blog</NavLink>
-        <NavLink href='/about'>About</NavLink>
+        <NavLink href='/car'>Cars</NavLink>
         <NavLink href='/community'>Community</NavLink>
+        <NavLink href='/about'>About</NavLink>
         <NavLink href='/car/share'>Share Car</NavLink>
-        <NavLink href='/car/random'>Any Car</NavLink>
+        {/* <NavLink href='/car/random'>Any Car</NavLink> */}
       </div>
     </nav>
   );
