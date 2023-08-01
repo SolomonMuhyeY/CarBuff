0bfa8ac431a314ce7a59333ac938f6839d27cfca
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-08-01 10:57:03 +0300

    adding error page

---
+++ app/errors.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <div className='error'>
+        <h1>Sorry There is Some Error 😢</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
