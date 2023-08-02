fdc6acbea092f3c5f34e43d29d3f17a1759fffa1
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-08-02 12:12:34 +0300

    adding getCar method

--- app/lib/cars.ts
+++ app/lib/cars.ts
@@ -1,6 +1,10 @@
 import sql from "better-sqlite3";
 
 const db = sql("cars.db");
-export function getCars() {
+export async function getCars() {
+  await new Promise((res, rej) => setTimeout(res, 2000));
   return db.prepare("SELECT * FROM cars").all();
+}
+export function getCar(slug: string) {
+  return db.prepare("SELECT * FROM cars WHERE slug = ?").get(slug);
 }
