75c664c497e117ca376c6d8f72b83ecec297e21f
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-08-06 18:53:48 +0300

    adding types

--- app/blog/[posts]/page.tsx
+++ app/blog/[posts]/page.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-const BlogDetailPages = ({ params }) => {
+interface Params {
+  posts: string;
+}
+
+const BlogDetailPages = ({ params }: { params: Params }) => {
   return (
     <div className='text-center my-8'>
       <h1 className='text-xl my-3'>Dynamic Routing...</h1>
