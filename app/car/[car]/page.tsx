031c1404d05afaee65d782f8c1d1bef54697a037
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-08-07 18:55:04 +0300

    adding types

--- app/car/[car]/page.tsx
+++ app/car/[car]/page.tsx
@@ -1,16 +1,11 @@
+// components/CarPage.tsx
 import Image from "next/image";
 import { getCar } from "@/app/lib/cars";
 import { notFound } from "next/navigation";
+import { CarProps, Car } from "@/types";
 
-const Page = ({
-  params,
-}: {
-  params: {
-    car: string;
-  };
-}) => {
-  // Ensure params.car is valid before calling getCar
-  const car = params.car ? getCar(params.car) : null;
+const Page = ({ params }: { params: CarProps }): JSX.Element => {
+  const car: Car | null = params.car ? (getCar(params.car) as Car) : null;
 
   if (!car) notFound();
 
@@ -19,30 +14,24 @@ const Page = ({
       <div className='max-w-3xl w-full bg-white rounded-lg shadow-lg overflow-hidden'>
         <div className='px-6 py-8'>
           <h1 className='text-3xl font-semibold text-gray-800 text-center mb-4'>
-            {car ? car.carName : "Car Not Found"}
+            {car.carName}
           </h1>
-          {car ? (
-            <div className='flex items-center mx-6 mb-4'>
-              <div className='relative w-96 h-96'>
-                <Image
-                  src={car.image}
-                  alt={car.carName}
-                  layout='fill'
-                  className='rounded-lg'
-                />
-              </div>
+          <div className='flex items-center mx-6 mb-4'>
+            <div className='relative w-96 h-96'>
+              <Image
+                src={car.imagePath}
+                alt={car.carName}
+                layout='fill'
+                className='rounded-lg'
+              />
             </div>
-          ) : null}
-          <p className='text-gray-600 text-lg mb-4'>
-            {car ? car.review : "No review available."}
-          </p>
+          </div>
+          <p className='text-gray-600 text-lg mb-4'>{car.review}</p>
           <div className='border-t border-gray-200 pt-4'>
             <p className='text-sm text-gray-500 mb-1'>
-              Created by {car ? car.creator : "Unknown"}
+              Created by {car.creator}
             </p>
-            <p className='text-sm text-gray-500'>
-              {car ? car.creator_email : "No email provided"}
-            </p>
+            <p className='text-sm text-gray-500'>{car.creator_email}</p>
             <a
               className='inline-block text-sm text-gray-500 px-6 py-2 border-2 border-lime-400 my-2 rounded-2xl'
               href={`mailto:${car.creator_email}`}
