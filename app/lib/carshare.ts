9f0b3f25b30ff50acd246d5925caaa4f9890722d
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-08-11 18:59:02 +0300

    adding post processor method

--- app/lib/carshare.ts
+++ app/lib/carshare.ts
@@ -1,12 +1,14 @@
 "use server";
-export async function shareCarPost(formData: FormData) {
-  const car = {
-    carName: formData.get("carName"),
-    review: formData.get("review"),
-    image: formData.get("image"),
-    creator: formData.get("creator"),
-    creator_email: formData.get("creator_email"),
-  };
 
-  console.log(car);
+import { redirect } from "next/navigation";
+import { saveCar } from "./cars";
+import { processFormData } from "./ProcessFormData";
+import { revalidatePath } from "next/cache";
+
+export async function shareCarPost(formData: FormData) {
+  const car = processFormData(formData);
+  // console.log(car);
+  await saveCar(car);
+  revalidatePath("/car");
+  redirect("/car");
 }
