c2621923b213901f59d1196180a04bf8795f8674
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-08-12 19:01:01 +0300

    modfying types

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,13 @@
-export type CarProps = {
-  slug: string;
+export interface Car {
+  slug?: string;
   carName: string;
-  image: string;
+  image: File; // Assuming image is a File object
+  imagePath: string; // New property to store the image path
   review: string;
   creator: string;
   creator_email: string;
-};
+}
+
+export interface CarProps {
+  car: string;
+}
